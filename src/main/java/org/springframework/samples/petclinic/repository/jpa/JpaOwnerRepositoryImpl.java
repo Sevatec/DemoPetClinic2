6a944aa26d9ae31bdbc09e548064db5d3c2c1e63
Sevatec <github@sevatec.com>
2015-04-30 15:23:22 -0400

    Turning off SQL Injection Test

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaOwnerRepositoryImpl.java
@@ -45,7 +45,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class JpaOwnerRepositoryImpl implements OwnerRepository {
 	//FLIP THIS VALUE TO DEMO SQL INJECTION ERRORS
-	private boolean sqlInjectionTest = true;
+	private boolean sqlInjectionTest = false;
 	
     @PersistenceContext
     private EntityManager em;
